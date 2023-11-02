9c05f51d4ccf0098d195c77141e01b8df5b0cb06
Gonzalo <gonzamartinez621@gmail.com>
2023-11-02 20:33:21 -0300

    Examen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,68 @@
 
+import { useState } from "react";
 import Card from "./Card";
 
 function App() {
+
+  const [nombreMascota, setNombreMascota] = useState('')
+  const [nombreDueno, setNombreDueno] = useState('')
+  const [showCard,setShowCard] = useState(false)
+  const [error,setError] = useState(false)
+
+  const handleChange = (event) => {
+    if (event.target.name === "nombreMascota") {
+      setNombreMascota(event.target.value);
+    } else if (event.target.name === "nombreDueno") {
+      setNombreDueno(event.target.value);
+    }
+  };
+
+  const handleEnviar = () => {
+    if (
+      nombreMascota.length >= 3 &&
+      nombreMascota.trim() !== "" &&
+      nombreDueno.length >= 6
+    ) {
+      setShowCard(true);
+      setError(false);
+    } else {
+      setShowCard(false);
+      setError(true);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+    <div className="App" style={{ margin: "500px" }}>
+      <h1>Registro de Mascotas</h1>
+      {!showCard ? (
+        <>
+          <input style={{margin: "20px"}}
+            type="text"
+            name="nombreMascota"
+            placeholder="Nombre de la mascota"
+            value={nombreMascota}
+            onChange={handleChange}
+          />
+
+          <input style={{margin: "20px"}}
+            type="text"
+            name="nombreDueno"
+            placeholder="Nombre de el dueño"
+            value={nombreDueno}
+            onChange={handleChange}
+          />
+
+          <button onClick={handleEnviar}>Enviar</button>
+        </>
+      ) : null}
+
+     {showCard ? (
+      
+        <Card mascota={nombreMascota} dueno={nombreDueno}  />
+      ) : (
+        error && <p>Por favor chequea que la información sea correcta.</p>
+      )}
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({ mascota, dueno }) => {
+  return (
+    <div style={{backgroundColor: "grey", color: "black", borderRadius:"5px"}}>
+      <h2 style={{margin:"20px"}}>Info de la Mascota Registrada</h2>
+      <h3 style={{margin:"20px"}}>{mascota} es de {dueno}</h3>
+    </div>
+  );
 };
 
 export default Card;
